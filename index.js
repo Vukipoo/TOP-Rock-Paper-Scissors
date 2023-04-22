fa5728a01c2e3c82f0be955133a3594c700470bd
Vukipoo <perovicv515@gmail.com>
2023-04-22 09:55:04 -0700

    Create function that generates random computer choice

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello World')
+let userChoice = '';
+
+
+function getComputerChoice(){
+    let items = ['rock', 'paper', 'scissors'];
+    let random = Math.floor(Math.random()*3);
+    let choice = items[random];
+    return choice;
+}
+
+console.log(getComputerChoice())
+
+
+/* if (userChoice === 'rock' && computerChoice ==='paper'){
+    console.log('Computer wins this round')
+}
+else if(userChoice === 'paper' && computerChoice === 'scissors'){
+    console.log('computer wins this round')
+}
+else if(userChoice === 'scissors' && computerChoice === 'rock'){
+    console.log('computer wins this round')
+}
+else {
+    console.log('Player wins this round')
+} */
