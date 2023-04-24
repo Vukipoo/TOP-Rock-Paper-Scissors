60ee44c10c705ec3d1b12ea59e7e6d751e3ad0e9
Vukipoo <perovicv515@gmail.com>
2023-04-24 13:51:03 -0700

    Finalize index.js

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ function getComputerChoice(){
     let choice = items[random];
     return choice;
 }
-
+     
+     computerSelection = getComputerChoice();
 
 
 function playRound (playerSelection, computerSelection){
-     playerSelection = playerChoice();
-     computerSelection = getComputerChoice();
+    
+    computerSelection = getComputerChoice();
 if (playerSelection === computerSelection) {
     return 'Its a tie'
 }
@@ -33,24 +34,34 @@ else {
 }
 }
 
-function playerChoice() {
-    let selection = prompt('Select your choice');
-    return selection
-}
+ 
 
 
 let computerScore = 0;
 let playerScore = 0;
 
-function game(){
-    
-    console.log(playRound(playerChoice(), getComputerChoice()))
-    console.log(playRound(playerChoice(), getComputerChoice()))
-    console.log(playRound(playerChoice(), getComputerChoice()))
-    console.log(playRound(playerChoice(), getComputerChoice()))
-    console.log(playRound(playerChoice(), getComputerChoice()))
-    console.log(declareWinner())
+function handleButtonClick(event) {
+    let playerSelection = event.target.id;
+    let computerChoice = getComputerChoice();
+    let result = playRound(playerSelection, computerChoice);
+
+    if(result.includes('player')) {
+        playerScore++
+    }
+    else if(result.includes('computer')){
+        computerScore++
+    }
+    playerScoreDisplay.innerHTML = `Player: ${playerScore}`
+    computerScoreDisplay.innerHTML = `Computer: ${computerScore}`
+    display2.innerHTML = result;
+
+
+    if (playerScore === 5 || computerScore === 5) {
+        display.innerHTML = declareWinner();
+  }
 }
+
+
 
 function declareWinner(){
     if (computerScore > playerScore){
@@ -63,4 +74,23 @@ function declareWinner(){
 }
 
 
-console.log(game())
+
+
+let rockBtn = document.querySelector('#rock')
+let paperBtn = document.querySelector('#paper')
+let scissorsBtn = document.querySelector('#scissors')
+
+rockBtn.addEventListener('click', handleButtonClick)
+paperBtn.addEventListener('click', handleButtonClick)
+scissorsBtn.addEventListener('click', handleButtonClick)
+
+let display = document.createElement('div');
+let display2 = document.createElement('div')
+let playerScoreDisplay = document.createElement('div')
+let computerScoreDisplay = document.createElement('div')
+let container = document.querySelector('.container')
+container.appendChild(display)
+container.appendChild(display2)
+container.appendChild(playerScoreDisplay)
+container.appendChild(computerScoreDisplay)
+
